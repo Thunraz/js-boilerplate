01493bf8f411cc62d066488d7b04573f9e4e29d5
Thunraz <thunraz@thunraz.com>
2017-10-27 08:03:13 +0200

    Added new task "zip" (useful for #js13kGames)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 const gulp = require('gulp');
 
-let tasks = ['build', 'css', 'template', 'watch', 'assets', 'electron'];
+let tasks = ['build', 'css', 'template', 'watch', 'assets', 'electron', 'zip'];
 
 // Bootstrap individual task files
 tasks.forEach(
