cd46356644ef409e42d0e67c54a18de7bcd78456
Julian <julian.dinges@men.de>
2018-07-06 08:29:04 +0200

    Rolling my own filesize detection

--- tasks/zip.js
+++ tasks/zip.js
@@ -1,21 +1,42 @@
 'use strict';
 
 let gulp     = require('gulp'),
-    filesize = require('gulp-filesize'),
+    gutil    = require('gulp-util'),
     rename   = require('gulp-rename'),
+    through  = require('through2'),
     uglify   = require('gulp-uglify-es').default,
     zip      = require('gulp-zip');
 
+function getZipSize(inKib) {
+    return through.obj(function(file, encoding, callback) {
+        let filenameShort = file.path.split(/\/|\\/).pop()
+
+        // Check if we're dealing with a file or a buffer
+        let byteSize = file.stat ? file.stat.size : Buffer.byteLength(String(file.contents));
+        
+        let formattedSize = byteSize;
+        if(inKib === true) {
+            formattedSize /= 1024;
+        }
+        formattedSize = formattedSize.toLocaleString();
+        formattedSize += inKib ? ' KiB' : ' B';
+
+        gutil.log('Size', gutil.colors.cyan(filenameShort), ':', gutil.colors.magenta(formattedSize));
+
+        callback(null, file);
+    });
+}
+
 module.exports = () => {
-    gulp.task('minify', ['build', 'css', 'template', 'assets'], () => {
+    gulp.task('minify', gulp.series(['build', 'css', 'template', 'assets'], () => {
         return gulp
             .src('dist/main.js')
             .pipe(uglify())
             .pipe(rename('main.min.js'))
             .pipe(gulp.dest('dist'));
-    });
+    }));
 
-    gulp.task('zip', ['minify'], () => {
+    gulp.task('zip', gulp.series('minify', () => {
         return gulp
             .src([
                 './dist/main.min.js',
@@ -24,7 +45,7 @@ module.exports = () => {
                 './dist/assets/**/*'
             ], { base: 'dist' })
             .pipe(zip('dist.zip'))
-            .pipe(filesize())
-            .pipe(gulp.dest('dist'));
-    });
+            .pipe(gulp.dest('dist'))
+            .pipe(getZipSize());
+    }));
 };
