450ce57f85044ecec2f970c345eff2216f6f78a5
Thunraz <thunraz@thunraz.com>
2018-07-06 13:15:36 +0200

    Added linting to task compile

--- tasks/compileScript.js
+++ tasks/compileScript.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const eslint = require('gulp-eslint');
+const gulp   = require('gulp');
 const rollup = require('rollup').rollup;
 
 let cache;
@@ -23,6 +25,24 @@ function build() {
     });
 }
 
-export default () => {
-    return build();
-};
+function lint() {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['src/js/**/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+}
+
+export default gulp.series(
+    build,
+    lint
+);
